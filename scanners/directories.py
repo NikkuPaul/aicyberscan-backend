f1d0b1f149e4464a5d98a7da4ba2507abdd9da16
Nikhil <paulnikhil881@gmail.com>
2026-03-28 05:32:46 +0100

    Update directories.py

--- scanners/directories.py
+++ scanners/directories.py
@@ -1,10 +1,12 @@
 import httpx
 
 async def scan_directories(domain: str):
-    url = f"https://api.hackertarget.com/httpheaders/?q={domain}"
+    url = f"http://web.archive.org/cdx/search/cdx?url={domain}/*&output=json&fl=original&collapse=urlkey"
     try:
         async with httpx.AsyncClient() as client:
-            r = await client.get(url, timeout=20)
-            return r.text
+            r = await client.get(url, timeout=25)
+            data = r.json()
+            urls = [row[0] for row in data[1:50]]  # limit to 50 URLs
+            return urls
     except:
-        return "Directory scan failed"
+        return []
