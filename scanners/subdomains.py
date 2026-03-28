d49cf57d6cf9eaa28ea55305ddf4206259c7e3c2
Nikhil <paulnikhil881@gmail.com>
2026-03-28 05:14:46 +0100

    Update subdomains.py

--- scanners/subdomains.py
+++ scanners/subdomains.py
@@ -1,18 +1,13 @@
-import os
 import httpx
-
-API_URL = os.getenv("https://api.hackertarget.com/hostsearch/?q=")
+import json
 
 async def find_subdomains(domain: str):
+    url = f"https://crt.sh/?q={domain}&output=json"
     try:
         async with httpx.AsyncClient() as client:
-            r = await client.get(f"{API_URL}{domain}", timeout=15)
-            text = r.text
-
-            # HackerTarget returns CSV lines: subdomain,IP
-            lines = text.splitlines()
-            subs = [line.split(",")[0] for line in lines if "," in line]
-
+            r = await client.get(url, timeout=20)
+            data = json.loads(r.text)
+            subs = list({entry["name_value"] for entry in data})
             return subs
     except:
         return []
