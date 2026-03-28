19b4b259815df760d1ebe12092bf3f82fd9013e3
Nikhil <paulnikhil881@gmail.com>
2026-03-28 05:17:41 +0100

    Update directories.py

--- scanners/directories.py
+++ scanners/directories.py
@@ -1,23 +1,10 @@
-import os
 import httpx
 
-API_KEY = os.getenv("https://urlscan.io/api/v1/scan")
-API_URL = os.getenv("019d321c-d271-770c-b5ad-0ff77cd87558")
-
 async def scan_directories(domain: str):
-    if not API_KEY or not API_URL:
-        return []
-
+    url = f"https://api.hackertarget.com/httpheaders/?q={domain}"
     try:
         async with httpx.AsyncClient() as client:
-            r = await client.get(
-                API_URL,
-                params={"domain": domain},
-                headers={"Authorization": f"Bearer {API_KEY}"},
-                timeout=45
-            )
-            data = r.json()
-            return data.get("paths", [])
+            r = await client.get(url, timeout=20)
+            return r.text
     except:
-        return []
-
+        return "Directory scan failed"
