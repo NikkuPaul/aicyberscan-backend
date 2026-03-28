5002c6f896935f440510a2a6a61dd07e2be36c21
Nikhil <paulnikhil881@gmail.com>
2026-03-28 05:16:54 +0100

    Update ports.py

--- scanners/ports.py
+++ scanners/ports.py
@@ -1,23 +1,10 @@
-import os
 import httpx
 
-API_KEY = os.getenv("qsJ37urm0JKptNhBkALpEFiT6wq12GeG")
-API_URL = os.getenv("https://api.shodan.io/shodan/host")
-
 async def scan_ports(domain: str):
-    if not API_KEY or not API_URL:
-        return []
-
+    url = f"https://api.hackertarget.com/nmap/?q={domain}"
     try:
         async with httpx.AsyncClient() as client:
-            r = await client.get(
-                API_URL,
-                params={"target": domain},
-                headers={"Authorization": f"Bearer {API_KEY}"},
-                timeout=45
-            )
-            data = r.json()
-            return data.get("open_ports", [])
+            r = await client.get(url, timeout=20)
+            return r.text
     except:
-        return []
-
+        return "Port scan failed"
